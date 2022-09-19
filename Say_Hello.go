af0cd433e0397cfc2968cc6062d6711c1d84ec86
reyhanhmdani <rey7dan7@gmail.com>
2022-09-19 15:43:56 +0700

    update Sayhello dengan parameter nama

--- Say_Hello.go
+++ Say_Hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello2() string {
-	return "WOIIIII UDIN"
+func SayHello2(name string) string {
+	return "WOIIIII UDIN" + name
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/reyhanhmdani/go_say_hello
+module github.com/reyhanhmdani/go_say_hello/v2
 
 go 1.19
